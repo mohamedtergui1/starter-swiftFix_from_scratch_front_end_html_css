54723474e4e563b13d81c5f52f187fcbe5912093
mohamed <simotergui4@gmail.com>
2023-10-21 17:24:17 +0200

    finish user store 4 <<menu drop down navigation >> ,finish user store 5 <<interactive  search bar icon>>, finish user store 6 testimonials automatic carousel

--- javascript/script.js
+++ javascript/script.js
@@ -9,11 +9,44 @@ icon.addEventListener('click', function() {
     aTig.classList.toggle("tige_a");
     bTig.classList.toggle("tige_b");
     cTig.classList.toggle("tige_c");
-
   });
 let iconSearch = document.querySelector("#icon_search");
-let iputSearch =document.querySelector("#Search_input");
+let inputSearch =document.querySelector("#Search_input");
 iconSearch.addEventListener('click',function() {
-  iputSearch.classList.toggle("open_search");
+  inputSearch.classList.toggle("open_search");
   iconSearch.classList.toggle("delete_padding_icon_search");
 });
+let buttonServices =document.querySelector("#drop_services_button_id");
+let dropservices = document.querySelector("#drop_services");
+// let buttonRect = buttonServices.getBoundingClientRect();
+// // const  buton_position = butonServices.getBoundingClientRect();
+// let buton_positionleft = buttonRect.offsetLeft  + window.scrollX;
+// let buton_positionright = buttonRect.offsetTop  + window.scrollY;
+// console.log(`Left: ${buton_positionleft}px, Top: ${buton_positionright}px`);
+buttonServices.addEventListener('click',function() {
+  dropservices.classList.toggle("afficher_services_droped");
+});
+if(window.innerWidth>=1023){
+  dropservices.style.position='absolute'
+} 
+
+// //////////////////////////////////////
+ 
+// Call the function
+ 
+// //////////////////////////////////////////////////////////
+ let longeur =document.querySelectorAll(".Testimonial_block")
+  console.log(longeur.length);
+ let delay_on_testimonialise = document.querySelector("#testimonials_id");
+  let i=0;
+  let x=1;
+ function toggleFlexDirection(){
+  if(i>longeur.length-2){x=-1;}
+  if(i<1) {x=1;}
+
+  delay_on_testimonialise.style.transform = `translateX(-${53.9 * i}rem)`;
+      setTimeout(toggleFlexDirection, 3000);
+      i+=x;
+    }
+
+    toggleFlexDirection();
